0ca7cc07069aaec8378fde282a90524c428d1bc3
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2023-06-05 02:19:05 +0200

    Added files and changes required for deplyment

--- app.js
+++ app.js
@@ -19,7 +19,11 @@ const app = express();
 //mongoose setup
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB="mongodb+srv://aneek:aneek786@cluster0.hpynao7.mongodb.net/local_library?retryWrites=true&w=majority";
+// Set up mongoose connection
+const dev_db_url =
+  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+
 
 main().catch((err) => console.log(err));
 async function main() {
