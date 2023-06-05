19590eaf608b68f54b8f983f96ef2bb51951ba4c
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2023-06-05 04:53:04 +0200

    tutorial done

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ async function main() {
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-app.use("/public", express.static(path.join(__dirname, 'public')));
+app.set('views', './src/public');
 
 app.use(logger('dev'));
 app.use(express.json());
