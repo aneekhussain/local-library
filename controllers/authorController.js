a36c0a66ad196cbd0d879ce7caaaceb7c1fce613
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2023-08-09 00:33:23 +0200

    done done

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -224,3 +224,22 @@ exports.author_update_post = [
     }
   }),
 ];
+
+
+exports.author_upload_image = [
+  uploadAuthorImage.single("image"),
+  
+  asyncHandler(async (req, res, next) => {
+    const author = await Author.findById(req.params.id).exec();
+
+    if (null===author) {
+      const err = new Error("Author not found");
+      err.status = 404;
+      return next(err);
+    }
+    //uodate author img in database
+    await Author.findByIdAndUpdate(req.params.id, {image: req.file.filename}, {});
+
+    res.redirect(author.url);
+  }),
+]
