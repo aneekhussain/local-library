985af7c0d12df7fdce59ab6daad69bf9ab5751e3
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2023-08-16 01:54:07 +0200

    img done?

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -6,17 +6,15 @@ const asyncHandler = require("express-async-handler");
 
 exports.search = asyncHandler(async (req, res, next ) => {
     const search = req.body.search.toString();
-    const [authors, books, genres, bookinstances ] = await Promise.all([
-        Author.find({first_name: {$regex: search}}), 
-        Book.find({title: {$regex: search}}),
+    const [authors, books] = await Promise.all([
+        Author.find({first_name: {$regex: search, $options: "i"}}), 
+        Book.find({title: {$regex: search, $options: "i"}}),
     ]);
 
     res.render("searching_list", {
         title: "Search",
         authors,
         books,
-        genres,
-        bookinstances,
     });
 });
 //Genre.find({name: {$regex: search}}),
