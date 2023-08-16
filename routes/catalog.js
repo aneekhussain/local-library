8eb7d0387539b0669133c4d4264c06c881e0327d
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2023-08-16 19:37:11 +0200

    img done?

--- routes/catalog.js
+++ routes/catalog.js
@@ -6,6 +6,7 @@ const book_controller = require("../controllers/bookController");
 const author_controller = require("../controllers/authorController");
 const genre_controller = require("../controllers/genreController");
 const book_instance_controller = require("../controllers/bookinstanceController");
+const search_controller = require("../controllers/searchController");
 
 
 //SEARCH ROUTE
