0c80a981fbf1babdbdb6f33d82ba4e81181c9b75
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2024-06-08 22:40:57 +0200

    exam prep 12

--- routes/catalog.js
+++ routes/catalog.js
@@ -18,7 +18,7 @@ router.post("/searching_list", search_controller.search);
 /// BOOK ROUTES ///
 
 // GET catalog home page.
-router.get("/catalog/books", book_controller.index);
+router.get("/", book_controller.index);
 
 // GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
 router.get("/book/create", book_controller.book_create_get);
