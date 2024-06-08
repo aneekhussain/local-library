3f3b23667e6026f429d8e0ac01eb08c9f3a6d1d8
aneekhussain <84678050+aneekhussain@users.noreply.github.com>
2024-06-08 22:15:10 +0200

    exam prep 10

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ mongoose.set("strictQuery", false);
 const dev_db_url ="mongodb+srv://aneek:aneek786@cluster0.hpynao7.mongodb.net/local_library?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
-main().catch((err) => console.log(err));
+main().catch((err) => console.log("database err:", err));
 async function main() {
   await mongoose.connect(mongoDB);
 }
